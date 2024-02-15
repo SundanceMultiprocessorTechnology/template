3e1795c1c1f9fd7d0100511c56de5181d5a5d5c7
EmilieWheatley <emilie.w@sundance.com>
2024-02-15 13:16:02 +0000

    update gitignore

--- BSP/2022.2/prebuilt/releases.sh
+++ BSP/2022.2/prebuilt/releases.sh
@@ -8,5 +8,7 @@ cp ../boot/BOOT.bin ./boot
 cp ../firmware/*.runs/impl_1/*.bit ./firmware
 cp ../firmware/*.xsa ./firmware
 cp ../petalinux/images/linux/* ./petalinux
+find ../software/baremetal -type f | grep -i ".*.elf$"  | xargs -I '{}' cp '{}' ./software/baremetal
+find ../software/linux -type f -executable  | xargs -I '{}' cp '{}' ./software/linux/
 
 
